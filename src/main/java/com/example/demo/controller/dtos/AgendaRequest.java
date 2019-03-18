c5c62098db471d8fc85f042acc2004aceed68181
neridev <rodrigogregorioneri@hotmail.com>
2019-03-18 14:02:14 -0300

    finalizando criar agendamento

---
+++ src/main/java/com/example/demo/controller/dtos/AgendaRequest.java
@@ -0,0 +1,64 @@
+package com.example.demo.controller.dtos;
+
+import java.sql.Date;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+public class AgendaRequest {
+
+	private String nome;
+	
+
+	private String email;
+	
+
+	private Date dataAgendamento;
+	
+
+	private Integer telefone;
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Date getDataAgendamento() {
+		return dataAgendamento;
+	}
+
+
+	public void setDataAgendamento(Date dataAgendamento) {
+		this.dataAgendamento = dataAgendamento;
+	}
+
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/domain/Agendamento.java
@@ -0,0 +1,76 @@
+package com.example.demo.domain;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Agendamento {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "data_agendamento")
+	private Date dataAgendamento;
+	
+	@Column(name = "telefone")
+	private Integer telefone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataAgendamento() {
+		return dataAgendamento;
+	}
+
+	public void setDataAgendamento(Date dataAgendamento) {
+		this.dataAgendamento = dataAgendamento;
+	}
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+
+}
